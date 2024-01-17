10e081514e754f5362cbd72ad99545e2c5fd798f
Tasneem <mohamedtasneem.22@ichat.sp.edu.sg>
2024-01-17 14:58:48 +0800

    added sound to the splash screen

--- app/src/main/java/com/sp/finvue/SplashScreen.java
+++ app/src/main/java/com/sp/finvue/SplashScreen.java
@@ -3,22 +3,27 @@ package com.sp.finvue;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.os.Handler;
 import android.view.WindowManager;
 
 public class SplashScreen extends AppCompatActivity {
 
+    private MediaPlayer mediaPlayer;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splashscreen);
+        mediaPlayer = MediaPlayer.create(this, R.raw.splashscreensound);
+        mediaPlayer.start();
 
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
+                mediaPlayer.release();
                 startActivity(new Intent(SplashScreen.this, LoginActivity.class));
                 finish();
             }
