b6ef06efecb1ad150ca199e639318d558965568a
Tasneem <mohamedtasneem.22@ichat.sp.edu.sg>
2024-02-02 23:20:22 +0800

    have done my implementation on homepage and is starting to implement astra db

--- app/src/main/java/com/sp/finvue/UserVolleyHelper.java
+++ app/src/main/java/com/sp/finvue/UserVolleyHelper.java
@@ -3,12 +3,14 @@ package com.sp.finvue;
 import java.util.HashMap;
 
 public class UserVolleyHelper {
-    static String region =  "https://e19129fa-47f2-40f7-ab7c-46539ea483d0-us-east-2.apps.astra.datastax.com/api/rest";
-    static String url = region + "/v2/namespaces/finvue/collections";
-    static String Cassandra_Token = "AstraCS:GdolkTDLTtGsvNRNuFnzzQuC";
+    static String region =  "https://753a59ec-6802-408c-8cb6-a2927c108807-us-east1.apps.astra.datastax.com/api/rest";
+    static String user_url = region + "/v2/keyspaces/transactions/user_table";
+    static String transaction_url = region + "/v2/keyspaces/transactions/transaction_table";
+    static String user_transaction_url = region + "/v2/keyspaces/transactions/user_transaction";
+    static String Cassandra_Token = "AstraCS:gmQSYCOULMhxQUADhtuZysTe";
     static int lastID = 0;
     static HashMap getHeader() {
-        HashMap<String, String> headers = new HashMap<>();
+        HashMap<String, String> headers = new HashMap<String, String>();
         headers.put("Content-Type", "application/json");
         headers.put("X-Cassandra-Token", Cassandra_Token);
         headers.put("Accept", "application/json");
