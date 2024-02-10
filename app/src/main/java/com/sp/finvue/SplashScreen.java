10eb7bb499a4686924052fa5c05c66d96b3d7256
Tasneem <mohamedtasneem.22@ichat.sp.edu.sg>
2024-02-10 22:14:23 +0800

    small change to splash screen

--- app/src/main/java/com/sp/finvue/SplashScreen.java
+++ app/src/main/java/com/sp/finvue/SplashScreen.java
@@ -27,6 +27,6 @@ public class SplashScreen extends AppCompatActivity {
                 startActivity(new Intent(SplashScreen.this, LoginActivity.class));
                 finish();
             }
-        }, 2500);
+        }, 3000);
     }
 }
